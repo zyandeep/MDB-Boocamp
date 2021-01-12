788440c63a0b363367ab0c42117c2d2c9356dece
zyandeep <zyandeep000@gmail.com>
2021-01-12 08:17:03 +0530

    2nd commit

--- day2/app.js
+++ day2/app.js
@@ -1,34 +1,101 @@
-const taskNodeList = document.querySelectorAll('.list-item');
+const taskArray = [];
+const ULlist = document.querySelector('.list ul');
+
+function updateTaskArray({ index, action }) {
+    if (action === 'del') {
+
+        taskArray.splice(index, 1);
+
+    } else {
+        taskArray[index].isDone = !taskArray[index].isDone;
+    }
+
+    // console.log(taskArray);
+    renderUI();
+}
 
 
-Array.from(taskNodeList).forEach(function (taskLi) {
+function attachEventHandlers() {
+    const taskNodeList = document.querySelectorAll('.list-item');
 
-    taskLi.addEventListener('click', function (event) {
+    Array.from(taskNodeList).forEach(function (taskLi) {
 
-        if (event.target.matches('.del-btn')) {
-            // Delete buttn clicked
+        taskLi.addEventListener('click', function (event) {
 
-            if (confirm('Are you sure about that?')) {
-                //    delete the task
+            if (event.target.matches('.del-btn')) {
+                // Delete buttn clicked
 
-                event.target.parentElement.remove();
+                if (confirm('Are you sure about that?')) {
+                    //    delete the task
+
+                    updateTaskArray({
+                        index: event.target.parentElement.dataset.li,
+                        action: 'del'
+                    });
+
+                    // event.target.parentElement.remove();
+                }
+
+            }
+            else if (event.target.matches('.list-item :first-child ')) {
+                // Task name clicked
+
+                updateTaskArray({
+                    index: event.target.parentElement.dataset.li,
+                    action: 'mod'
+                });
+
+                // event.target.classList.toggle('mark-done');
             }
 
-        }
-        else if (event.target.matches('.list-item :first-child ')) {
-            // Task name clicked
+        });
 
-            event.target.classList.toggle('mark-done');
-        }
+    })
+}
 
+
+function renderUI() {
+    let listContentStr = '';
+
+    taskArray.forEach(function (value, index) {
+        listContentStr += `
+        
+                <li class="list-item" data-li="${index}">
+                    <span class="${value.isDone ? 'mark-done' : ''}">${value.name}</span>
+                    <span class="del-btn">&times; </span>
+                </li>
+        
+        `;
     });
 
-})
+    ULlist.innerHTML = listContentStr;
+    attachEventHandlers();
+}
 
 
-document.querySelector('form').addEventListener('submit', function (event) {
-
+function addTask(event) {
     event.preventDefault();
 
+    const taskValue = document.querySelector('input[type="text"]').value.trim();
 
+    if (taskValue === '') {
+        alert('Please add a task');
+        return;
+    }
+
+    taskArray.push({
+        name: taskValue,
+        isDone: false
+    });
+
+    renderUI();
+}
+
+
+document.querySelector('form a').addEventListener('click', function (event) {
+    addTask(event);
+});
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    addTask(event);
 });
